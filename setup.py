9a289328bc78e1457c70797664b406288ad68af7
Deep Ganguli <dganguli@stitchfix.com>
2015-06-03 09:15:52 -0700

    setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='deeplearning',
+    version='0.0.1',
+    author='Deep Ganguli',
+    author_email='deep.ganguli@gmail.com',
+    packages=[
+        'deeplearning'
+    ],
+    description='Methods for exploring and understanding neural networks and deep learning',
+    install_requires=['numpy']
+)
+
